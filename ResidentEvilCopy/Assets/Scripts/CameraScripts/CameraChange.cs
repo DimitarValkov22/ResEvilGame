3bdb9da40e0889f35420d52304b5d59e05853abb
Dimitar Valkov <mitaka_1911@abv.bg>
2021-07-22 22:14:11 +0300

    22.07.2021
    1.Importing character
    2.Character movement scripts

---
+++ ResidentEvilCopy/Assets/Scripts/CameraScripts/CameraChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour
+{
+    public GameObject theCamera01;
+    public GameObject theCamera02;
+    public bool camOn = false;
+    public int cameraNumber;
+    
+    void Start()
+    {
+        cameraNumber = 1;
+        StartCoroutine(CameraSwitch());
+    }
+
+    IEnumerator CameraSwitch()
+    {
+        yield return new WaitForSecondsRealtime(5);
+        theCamera02.SetActive(true);
+        theCamera01.SetActive(false);
+        camOn = true;
+        cameraNumber = 2;
+    }
+
+}
---
+++ ResidentEvilCopy/Assets/Scripts/CharacterScripts/TankControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankControl : MonoBehaviour
+{
+    public GameObject thePlayer;
+    public bool isMoving;
+    public float horizontalMove;
+    public float verticalMove;
+    
+    void Update()
+    {
+        if(Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            isMoving = true;
+            thePlayer.GetComponent<Animator>().Play("Walk");
+            horizontalMove = Input.GetAxis("Horizontal") * Time.deltaTime * 150;
+            verticalMove = Input.GetAxis("Vertical") * Time.deltaTime * 4;
+            thePlayer.transform.Rotate(0, horizontalMove, 0);
+            thePlayer.transform.Translate(0, 0, verticalMove);
+        }
+        else
+        {
+            isMoving = false;
+            thePlayer.GetComponent<Animator>().Play("Idle");
+        }
+    }
+}
