4a755dbb3c6d51e73217ee7a9386612c574412bf
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-02 19:08:33 +0300

    2.8.2021
    1.Inventory control

---
+++ ResidentEvilCopy/Assets/Scripts/UI/InventoryControll.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryControll : MonoBehaviour
+{
+    public GameObject theInventory;
+    public GameObject FadeIn;
+    public AudioSource MenuOpen;
+    public bool isOpen = false;
+    public AudioSource MenuClose;
+    public bool canClose = false;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Cancel") && isOpen == false && canClose == false)
+        {
+            isOpen = true;
+            MenuOpen.Play();
+            FadeIn.SetActive(true);
+            StartCoroutine(InvControll());
+        }
+        if(Input.GetButton("Cancel") && isOpen == true && canClose == true)
+        {
+            Time.timeScale = 1;
+            Cursor.visible = false ;
+            isOpen = false;
+            MenuClose.Play();
+            FadeIn.SetActive(true);
+            StartCoroutine(InvControll());
+        }
+    }
+
+    public void ExitButton()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        isOpen = false;
+        MenuClose.Play();
+        FadeIn.SetActive(true);
+        StartCoroutine(InvControll());
+    }
+
+    IEnumerator InvControll()
+    {
+        yield return new WaitForSecondsRealtime(0.25f);
+        if(isOpen == true)
+        {
+            theInventory.SetActive(true);
+        }
+        else
+        {
+            theInventory.SetActive(false);
+        }
+        yield return new WaitForSecondsRealtime(0.25f);
+        FadeIn.SetActive(false);
+        if (isOpen == true)
+        {
+            canClose = true;
+            Time.timeScale = 0;
+            Cursor.visible = true;
+        }
+        else
+        {
+            canClose = false;
+        }
+    }
+}
