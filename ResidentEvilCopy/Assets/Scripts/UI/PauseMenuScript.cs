ef4eaeaa56eb869374fc63a6d40b0f3958b01fc4
Dimitar Valkov <mitaka_1911@abv.bg>
2021-08-03 19:17:37 +0300

    3.8.2021

---
+++ ResidentEvilCopy/Assets/Scripts/UI/PauseMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    public bool gamePaused = false;
+    public AudioSource mainMusic;
+    public GameObject pauseScreen;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            if(gamePaused == false)
+            {
+                Time.timeScale = 0;
+                gamePaused = true;
+                mainMusic.Pause();
+                pauseScreen.SetActive(true);
+            }
+            else
+            {
+                pauseScreen.SetActive(false);
+                mainMusic.UnPause();
+                gamePaused = false;
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
